dd18bd7b9ca34601d3a093280d30313437523fd1
basilspice <basil.breton@gmail.com>
2024-01-24 21:47:17 -0800

    changing tokens

--- pages/api/generate-recipe.js
+++ pages/api/generate-recipe.js
@@ -19,10 +19,10 @@ export default async function (req, res) {
   const completion = await openai.createCompletion({
     model: "gpt-3.5-turbo-instruct",
     prompt: prompt,
-    temperature: 0.6,
-    max_tokens: 2024,
+    temperature: 0.5,
+    max_tokens: 3032,
   });
-  res.status(500).json({ result: completion.data.choices[0].text });
+  res.status(200).json({ result: completion.data.choices[0].text });
 }
 
 function generatePrompt(dishStyle, ingredients, complexity, restrictions) {
